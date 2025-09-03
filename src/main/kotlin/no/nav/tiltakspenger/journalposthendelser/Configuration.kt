c3f92edd678495807679f0d06b15a16da3f60a41
ramziabuqassim <ramzi.abu.qassim@nav.no>
2025-09-03 15:13:42 +0200

    Update logback configuration file path retrieval in Configuration.kt

--- src/main/kotlin/no/nav/tiltakspenger/journalposthendelser/Configuration.kt
+++ src/main/kotlin/no/nav/tiltakspenger/journalposthendelser/Configuration.kt
@@ -16,7 +16,7 @@ object Configuration {
     val kafkaKeystorePath = getEnvVar("KAFKA_KEYSTORE_PATH")
     val kafkaCredstorePassword = getEnvVar("KAFKA_CREDSTORE_PASSWORD")
     val applicationHttpPort = getEnvVar("PORT", 8084.toString()).toInt()
-    val logbackConfigFile = if (isNais()) "logback.xml" else "logback.local.xml"
+    val logbackConfigFile = getEnvVar("LOGBACK_CONFIG_FILE", "logback.local.xml")
 
     fun isNais() = applicationProfile() != Profile.LOCAL
 
