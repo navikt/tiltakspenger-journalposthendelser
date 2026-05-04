65aae7b2d9182e3493c68435fd93a06c57c0c890
John Andre Hestad <john.andre.hestad@nav.no>
2026-05-04 15:17:45 +0200

    Oppdater fellesLibs

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 val jvmVersion = JvmTarget.JVM_21
 val mainClassFile = "no.nav.tiltakspenger.journalposthendelser.ApplicationKt"
 
-val felleslibVersion = "0.0.758"
+val felleslibVersion = "0.0.781"
 val ktorVersion = "3.4.3"
 val confluentVersion = "8.1.1"
 val avroVersion = "1.12.1"
