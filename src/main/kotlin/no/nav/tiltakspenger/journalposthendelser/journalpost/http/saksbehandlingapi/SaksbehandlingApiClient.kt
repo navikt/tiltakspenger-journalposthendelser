d9484fe08cc6e6f425f2ae376c765435177efbdd
John Andre Hestad <john.andre.hestad@nav.no>
2026-07-11 14:58:53 +0200

    Forbedre kdoc-lenker i httpklienter

--- src/main/kotlin/no/nav/tiltakspenger/journalposthendelser/journalpost/http/saksbehandlingapi/SaksbehandlingApiClient.kt
+++ src/main/kotlin/no/nav/tiltakspenger/journalposthendelser/journalpost/http/saksbehandlingapi/SaksbehandlingApiClient.kt
@@ -14,6 +14,15 @@ import io.ktor.http.contentType
 import no.nav.tiltakspenger.libs.common.AccessToken
 import no.nav.tiltakspenger.libs.common.CorrelationId
 
+/**
+ * HTTP-klient for tiltakspenger-saksbehandling-api (egen tjeneste).
+ *
+ * Kildekode: https://github.com/navikt/tiltakspenger-saksbehandling-api
+ * Dokumentasjon: README-en i kildekode-repoet
+ * API-spec: -
+ * Slack: #tiltakspenger-værsågod (eget team)
+ * Teamkatalog: https://teamkatalogen.nav.no/team/15bca3d2-2584-4167-85ba-faab1f1cfb53
+ */
 class SaksbehandlingApiClient(
     private val httpClient: HttpClient,
     private val basePath: String,
