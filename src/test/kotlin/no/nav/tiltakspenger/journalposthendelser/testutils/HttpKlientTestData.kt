019c2668feeac8ec94e558f1ba388e5b1495facf
John Andre Hestad <john.andre.hestad@nav.no>
2026-08-04 23:49:35 +0200

    Oppdater HttpKlientTestData.kt med nye metadata for HTTP-klient og juster felleslibVersion i build.gradle.kts

--- src/test/kotlin/no/nav/tiltakspenger/journalposthendelser/testutils/HttpKlientTestData.kt
+++ src/test/kotlin/no/nav/tiltakspenger/journalposthendelser/testutils/HttpKlientTestData.kt
@@ -4,9 +4,13 @@ import no.nav.tiltakspenger.libs.common.AccessToken
 import no.nav.tiltakspenger.libs.common.fixedClock
 import no.nav.tiltakspenger.libs.httpklient.HttpKlientMetadata
 import no.nav.tiltakspenger.libs.httpklient.HttpKlientTidsstempler
+import no.nav.tiltakspenger.libs.httpklient.Tidsgrenser
+import no.nav.tiltakspenger.libs.httpklient.UriSynlighet
 import no.nav.tiltakspenger.libs.httpklient.infra.kall.AuthTokenProvider
+import java.net.URI
 import java.time.Instant
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 
 val testTokenProvider = object : AuthTokenProvider {
     override suspend fun hentToken(skipCache: Boolean) = AccessToken("token", Instant.now(fixedClock).plusSeconds(3600))
@@ -14,6 +18,10 @@ val testTokenProvider = object : AuthTokenProvider {
 
 /** Minimal metadata til feiltyper i tester som ikke bryr seg om HTTP-detaljene. */
 fun tomHttpKlientMetadata(statusCode: Int? = 200) = HttpKlientMetadata(
+    method = "POST",
+    uri = URI.create("https://example.test/endepunkt"),
+    uriSynlighet = UriSynlighet.VanligLogg,
+    tidsgrenser = Tidsgrenser(svar = 30.seconds, oppkobling = 10.seconds),
     rawRequestString = "{}",
     rawResponseString = "{}",
     requestHeaders = emptyMap(),
