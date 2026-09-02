f3c4275111ff59e753f25099fb05953967b92bf9
Anders Nomerstad <anders@nomerstad.com>
2026-09-02 20:15:20 +0200

    Kun syntetiske fnr i tester + konsist-test for å verifisere det

---
+++ src/test/kotlin/no/nav/tiltakspenger/journalposthendelser/testutils/TestFnr.kt
@@ -0,0 +1,9 @@
+package no.nav.tiltakspenger.journalposthendelser.testutils
+
+import no.nav.tiltakspenger.libs.common.FnrGenerator
+
+/**
+ * Delt generator for syntetiske fødselsnumre i tester.
+ * Hvert kall til [generer] gir et nytt, unikt fnr.
+ */
+val fnrGenerator = FnrGenerator()
