a0a6c61783c6dac6e7cd450945af8e1258690c29
John Andre Hestad <john.andre.hestad@nav.no>
2026-09-09 17:28:58 +0200

    Bytt til io.github.ben-manes.versions tp-bau

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,7 @@ plugins {
     kotlin("jvm") version "2.4.20"
     kotlin("plugin.serialization") version "2.4.20"
     id("com.diffplug.spotless") version "8.10.1"
-    id("com.github.ben-manes.versions") version "0.61.0"
+    id("io.github.ben-manes.versions") version "0.61.0"
     // https://github.com/androa/gradle-plugin-avro
     id("io.github.androa.gradle.plugin.avro") version "0.0.12"
     id("org.jetbrains.kotlinx.kover") version "0.9.9"
